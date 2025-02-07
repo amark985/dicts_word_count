c343a73dca24299907e156d8d820580f1d6d9412
ASHLEIGH_MARKgit config --global user.email ASHLEIGH.MARK27@GMAIL.COM <ashleigh.mark27@gmail.com>
2025-02-06 21:57:42 -0500

    Working Code - Word Count

--- wordcount.py
+++ wordcount.py
@@ -1,17 +1,16 @@
 """Count words in file."""
 
-read_file = open("test.txt")
+read_file = open("twain.txt")
 
 word_count = {}
 
 # Loop through the text
 for line in read_file:
     word_splits = line.split(' ')
-    print(word_splits)
-
+    
     # Loop through the words in each line
     for word in word_splits:
         word_count[word] = word_count.get(word, 0) + 1
 
 
-return word_count
+print(word_count)
